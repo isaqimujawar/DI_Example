fd64e834c713617a1b7837250a81a65903515185
isaq-hp <isaqimujawar>
2022-06-26 02:35:56 +0530

    Adding Hilt Dependencies (took more than 2 hours to figure out the dependencies and resolve the errors)

--- app/src/main/java/com/example/diexample/login/LoginFragment.kt
+++ app/src/main/java/com/example/diexample/login/LoginFragment.kt
@@ -1,27 +1,15 @@
 package com.example.diexample.login
 
-import android.app.Application
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import com.example.diexample.MyApplication
 import com.example.diexample.R
 import com.example.diexample.databinding.FragmentLoginBinding
-import com.example.diexample.login.model.LoginRetrofitService
-import com.example.diexample.login.model.UserLocalDataSource
-import com.example.diexample.login.model.UserRemoteDataSource
-import com.example.diexample.login.model.UserRepository
 import com.example.diexample.login.viewmodel.LoginViewModel
 
 class LoginFragment : Fragment(R.layout.fragment_login) {
-
-    companion object {
-        fun newInstance() = LoginFragment()
-    }
-
     private lateinit var viewModel: LoginViewModel
     private var _binding: FragmentLoginBinding? = null
 
@@ -36,8 +24,8 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
         val appContainer = (activity?.application as MyApplication).appContainer
 
         // Lastly, create an instance of LoginViewModel with userRepository
-        viewModel = ViewModelProvider(this).get(LoginViewModel(appContainer.userRepository)::class.java)
-        //viewModel.setUserRepository(appContainer.userRepository)      // Field Injection (or Setter Injection)
+        viewModel = ViewModelProvider(this).get(LoginViewModel()::class.java)
+        viewModel.setUserRepository(appContainer.userRepository)      // Field Injection (or Setter Injection)
         binding.tvGreet.text = viewModel.getGreetings()
     }
 
--- app/src/main/java/com/example/diexample/login/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/example/diexample/login/viewmodel/LoginViewModel.kt
@@ -4,12 +4,11 @@ import androidx.lifecycle.ViewModel
 import com.example.diexample.login.model.IUserRepository
 import com.example.diexample.login.model.UserRepository
 
-class LoginViewModel(private val userRepository: IUserRepository) : ViewModel() {
-    /*
+class LoginViewModel() : ViewModel() {
     // We can use Field Injection (or Setter Injection) if we don't have access to the constructor of the class
     fun setUserRepository(userRepository: IUserRepository){
 
-    }*/
+    }
 
     fun getGreetings() = "Hello User"
 
